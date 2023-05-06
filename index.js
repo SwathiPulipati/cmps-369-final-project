515e42d9f0074ba0ba03e35ea3d23d2fc0a42230
Swathi Pulipati <swathip1007@gmail.com>
2023-05-06 15:00:57 -0400

    test commit

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// const Database = require('./db');
-// const db = new Database();
-// db.initialize();
+const Database = require('./db');
+const db = new Database();
+db.initialize();
 
 const app = express();
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-// app.use((req, res, next) => {
-//     req.db = db;
-//     next();
-// });
+app.use((req, res, next) => {
+    req.db = db;
+    next();
+});
 
-app.listen(8080, () => {
-    console.log("Listening on port 8080");
+app.use('/contacts', require('./routes/contacts'))
+app.use('/accounts', require('./routes/accounts'))
+app.use('/', (req, res) => {
+    res.render('home', {contacts: []});
+});
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
 })
